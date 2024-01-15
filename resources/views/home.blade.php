bc282a8e8ccc7d4be1653cbb40d1daad97b30754
Taufiq <htaufiq225@gmail.com>
2024-01-15 20:25:35 +0800

    ready to deploy

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -85,8 +85,8 @@
                     Let's talk
                 </a>
             </div>
-            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup">
+            <div class="hidden lg:mt-0 lg:col-span-4 lg:flex">
+                <img src="{{ asset('img/coding.svg') }}" alt="mockup">
             </div>
         </div>
     </section>
