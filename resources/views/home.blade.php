5301cc3905693ca2bf6d241faa1e468f90ed3b9f
Taufiq <htaufiq225@gmail.com>
2024-01-16 17:44:43 +0800

    memperbaiki hero section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,7 +84,7 @@
     <section id="home" class="bg-white dark:bg-gray-900">
         <div class="grid max-w-screen-xl h-screen px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="mr-auto place-self-center lg:col-span-7">
-                <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Web Development</h1>
+                <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Web Developer</h1>
                 <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Skilled web developer specializing in creating dynamic and user-friendly websites. Explore my portfolio for more</p>
                 <a href="#about" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                     Get started
